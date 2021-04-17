85206192936495524a289971d8d91852843aa91c
Paulo4025 <30624927+Paulo4025@users.noreply.github.com>
2021-04-17 00:07:10 -0300

    commit

--- base-frontend/src/app/app.module.ts
+++ base-frontend/src/app/app.module.ts
@@ -11,11 +11,10 @@ import { LandingComponent } from "./landing/landing.component";
 import { ProfileComponent } from "./../../plugins/plugin-core/plugin-core-frontend/src/app/profile/profile.component";
 import { NavbarComponent } from "./shared/navbar/navbar.component";
 import { FooterComponent } from "./shared/footer/footer.component";
-
 import { HomeModule } from "./home/home.module";
 import { LoginComponent } from "./../../plugins/plugin-login/plugin-login-frontend/src/app/login/login.component";
-import { UtilFuncoes } from "plugins/plugin-util-common/util-common-frontend/src/app/util-funcoes";
-import { UtilConstant } from "plugins/plugin-util-common/util-common-frontend/src/app/util-constants";
+import { Funcoes } from "plugins/plugin-common/plugin-common-frontend/src/app/funcoes";
+import { Constants } from "plugins/plugin-common/plugin-common-frontend/src/app/constants";
 import { MatSliderModule } from "@angular/material/slider";
 import { MatFormFieldModule } from "@angular/material/form-field";
 import { ReactiveFormsModule } from "@angular/forms";
@@ -26,17 +25,17 @@ import { MatSnackBarModule } from "@angular/material/snack-bar";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { Configuration } from "configuration/ambient/configuration";
 import { ServiceConfig } from "plugins/plugin-login/plugin-login-frontend/src/app/service/ServiceConfig";
-import { UtilService } from "plugins/plugin-util-common/util-common-frontend/src/app/util-service";
+import { HttpService } from "plugins/plugin-common/plugin-common-frontend/src/app/service";
 import { HttpClientModule } from "@angular/common/http";
-import { UtilLoader } from "plugins/plugin-util-common/util-common-frontend/src/app/util-loader/util-loader";
-import { UtilDefinicoes } from "plugins/plugin-util-common/util-common-frontend/src/app/util-definicoes";
+import { Loader } from "plugins/plugin-common/plugin-common-frontend/src/app/loader/loader";
+import { Definicoes } from "plugins/plugin-common/plugin-common-frontend/src/app/definicoes";
 import { Service } from "plugins/plugin-login/plugin-login-frontend/src/app/service/Service";
 import { MatNativeDateModule, MAT_DATE_LOCALE } from "@angular/material/core";
 import { MatSelectModule } from "@angular/material/select";
 import { RecuperarSenhaComponent } from "plugins/plugin-login/plugin-login-frontend/src/app/recuperar-senha/recuperar-senha.component";
-import { PreRegistrarEmpresaComponent } from "plugins/plugin-util-common/util-common-frontend/src/app/modal/pre-registrar-empresa/pre-registrar-empresa/pre-registrar-empresa.component";
+import { PreRegistrarEmpresaComponent } from "plugins/plugin-common/plugin-common-frontend/src/app/modal/pre-registrar-empresa/pre-registrar-empresa/pre-registrar-empresa.component";
 import { MatCardModule } from "@angular/material/card";
-import { DadosAcessoUsuarioComponent } from "plugins/plugin-util-common/util-common-frontend/src/app/modal/dados-acesso-usuario/dados-acesso-usuario.component";
+import { DadosAcessoUsuarioComponent } from "plugins/plugin-common/plugin-common-frontend/src/app/modal/dados-acesso-usuario/dados-acesso-usuario.component";
 import { MatExpansionModule } from "@angular/material/expansion";
 import { MatTableModule } from "@angular/material/table";
 import { ListaLojasProprietarioComponent } from "plugins/plugin-core/plugin-core-frontend/src/app/components/lista-lojas-proprietario/lista-lojas-proprietario.component";
@@ -45,7 +44,7 @@ import { MatPaginatorModule } from "@angular/material/paginator";
 import { ProfileEmpresaComponent } from "plugins/plugin-core/plugin-core-frontend/src/app/profile-empresa/profile-empresa.component";
 import { DadosEmpresaProfileComponent } from "plugins/plugin-core/plugin-core-frontend/src/app/profile-empresa/dados-empresa-profile/dados-empresa-profile.component";
 import { ListaFuncionarioEmpresaComponent } from "plugins/plugin-core/plugin-core-frontend/src/app/profile-empresa/lista-funcionario-empresa/lista-funcionario-empresa.component";
-import { PreRegistrarFuncionarioComponent } from "plugins/plugin-util-common/util-common-frontend/src/app/modal/pre-registrar-funcionario/pre-registrar-funcionario.component";
+import { PreRegistrarFuncionarioComponent } from "plugins/plugin-common/plugin-common-frontend/src/app/modal/pre-registrar-funcionario/pre-registrar-funcionario.component";
 import {MatCheckboxModule} from '@angular/material/checkbox';
 import {MatDividerModule} from '@angular/material/divider';
 import {MatListModule} from '@angular/material/list';
@@ -59,7 +58,7 @@ import {MatListModule} from '@angular/material/list';
     FooterComponent,
     LoginComponent,
     RecuperarSenhaComponent,
-    UtilLoader,
+    Loader,
     PreRegistrarEmpresaComponent,
     DadosAcessoUsuarioComponent,
     ListaLojasProprietarioComponent,
@@ -95,14 +94,14 @@ import {MatListModule} from '@angular/material/list';
   ],
   entryComponents: [PreRegistrarEmpresaComponent, DadosAcessoUsuarioComponent],
   providers: [
-    UtilConstant,
-    UtilFuncoes,
+    Constants,
+    Funcoes,
     Configuration,
     ServiceConfig,
-    UtilService,
-    UtilDefinicoes,
-    UtilLoader,
     Service,
+    Definicoes,
+    Loader,
+    HttpService,
     { provide: MAT_DATE_LOCALE, useValue: "pt-BR" },
   ],
   bootstrap: [AppComponent],
--- base-frontend/src/app/shared/navbar/navbar.component.ts
+++ base-frontend/src/app/shared/navbar/navbar.component.ts
@@ -1,61 +1,63 @@
-import { Component, OnInit } from '@angular/core';
-import { Router, NavigationEnd, NavigationStart } from '@angular/router';
-import { Location, PopStateEvent } from '@angular/common';
-import { UtilFuncoes } from 'plugins/plugin-util-common/util-common-frontend/src/app/util-funcoes';
+import { Component, OnInit } from "@angular/core";
+import { Router, NavigationEnd, NavigationStart } from "@angular/router";
+import { Location, PopStateEvent } from "@angular/common";
+import { Funcoes } from "plugins/plugin-common/plugin-common-frontend/src/app/funcoes";
 
 @Component({
-    selector: 'app-navbar',
-    templateUrl: './navbar.component.html',
-    styleUrls: ['./navbar.component.scss']
+  selector: "app-navbar",
+  templateUrl: "./navbar.component.html",
+  styleUrls: ["./navbar.component.scss"],
 })
 export class NavbarComponent implements OnInit {
-    public isCollapsed = true;
-    private lastPoppedUrl: string;
-    private yScrollStack: number[] = [];
+  public isCollapsed = true;
+  private lastPoppedUrl: string;
+  private yScrollStack: number[] = [];
 
-    constructor(public location: Location, private router: Router, private utilFuncoes: UtilFuncoes) {
-    }
+  constructor(
+    public location: Location,
+    private router: Router,
+    private funcoes: Funcoes
+  ) {}
 
-    ngOnInit() {
-        this.router.events.subscribe((event) => {
-            this.isCollapsed = true;
-            if (event instanceof NavigationStart) {
-                if (event.url != this.lastPoppedUrl)
-                    this.yScrollStack.push(window.scrollY);
-            } else if (event instanceof NavigationEnd) {
-                if (event.url == this.lastPoppedUrl) {
-                    this.lastPoppedUrl = undefined;
-                    window.scrollTo(0, this.yScrollStack.pop());
-                } else
-                    window.scrollTo(0, 0);
-            }
-        });
-        this.location.subscribe((ev: PopStateEvent) => {
-            this.lastPoppedUrl = ev.url;
-        });
-    }
+  ngOnInit() {
+    this.router.events.subscribe((event) => {
+      this.isCollapsed = true;
+      if (event instanceof NavigationStart) {
+        if (event.url != this.lastPoppedUrl)
+          this.yScrollStack.push(window.scrollY);
+      } else if (event instanceof NavigationEnd) {
+        if (event.url == this.lastPoppedUrl) {
+          this.lastPoppedUrl = undefined;
+          window.scrollTo(0, this.yScrollStack.pop());
+        } else window.scrollTo(0, 0);
+      }
+    });
+    this.location.subscribe((ev: PopStateEvent) => {
+      this.lastPoppedUrl = ev.url;
+    });
+  }
 
-    finalizarSessaoUsuario() {
-        this.utilFuncoes.finalizarSesssaoUsuario() === true ? this.utilFuncoes.routerRedirect("/login") : null
-    }
+  finalizarSessaoUsuario() {
+    return this.funcoes.finalizarSesssaoUsuario() === true
+      ? this.funcoes.routerRedirect("/login")
+      : null;
+  }
 
-    isHome() {
-        var titlee = this.location.prepareExternalUrl(this.location.path());
+  isHome() {
+    var titlee = this.location.prepareExternalUrl(this.location.path());
 
-        if (titlee === '#/home') {
-            return true;
-        }
-        else {
-            return false;
-        }
+    if (titlee === "#/home") {
+      return true;
+    } else {
+      return false;
     }
-    isDocumentation() {
-        var titlee = this.location.prepareExternalUrl(this.location.path());
-        if (titlee === '#/documentation') {
-            return true;
-        }
-        else {
-            return false;
-        }
+  }
+  isDocumentation() {
+    var titlee = this.location.prepareExternalUrl(this.location.path());
+    if (titlee === "#/documentation") {
+      return true;
+    } else {
+      return false;
     }
+  }
 }
